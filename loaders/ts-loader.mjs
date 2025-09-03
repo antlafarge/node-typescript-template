106293a1c2493e7734442934049f04ab64259041
Antoine Lafarge <ant.lafarge@gmail.com>
2025-09-03 18:35:08 +0200

    .

--- loaders/ts-loader.mjs
+++
@@ -1,6 +0,0 @@
-// ts-loader.mjs
-import { register } from 'node:module';
-import { pathToFileURL } from 'node:url';
-
-// Register ts-node/esm as a loader for TypeScript in ESM mode
-register('ts-node/esm', pathToFileURL('./'));
