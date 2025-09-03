0f7750d70c0066512f0c47ad367d9e20f61f7ee8
Antoine Lafarge <ant.lafarge@gmail.com>
2025-09-03 18:33:30 +0200

    Use tsx

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-import express from 'express';
 import data from './data.json' with { type: "json" };
 import MyModule from './myModule.js';
 
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send(MyModule.helloWorld)
-});
-
-console.log(`http://localhost:${data.port}`);
-
-app.listen(data.port)
+console.log(MyModule.helloWorld, data.port);
